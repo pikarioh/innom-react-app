497fd59b4c2ac7b42736d43df881c638d9fba994
Thanathon Holm <32491527+pikarioh@users.noreply.github.com>
2021-12-07 21:01:20 +0100

    assets updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import AwesomeSlider from 'react-awesome-slider'
 import AwesomeSliderStyles from 'react-awesome-slider/src/styles'
 import "./Header.css"
 import header1 from './assets/header_01.jpg'
-import header2 from './assets/header_02.JPG'
+import header2 from './assets/header_02.jpg'
 import header3 from './assets/header_03.JPG'
 
 const Header = () => {
     return (
         <AwesomeSlider cssModule={AwesomeSliderStyles}>
-                <div data-src={header1} />
                 <div data-src={header2} />
+                <div data-src={header1} />
                 <div data-src={header3} />
         </AwesomeSlider>
     )
